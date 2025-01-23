4043880c16d7f02bdecff4f38c3dd4a2e4a829d6
Cristian Sánchez <co.cristiand@gmail.com>
2025-01-23 10:26:12 -0500

    Mejor experiencia de usuario

--- app.js
+++ app.js
@@ -2,32 +2,56 @@
 let amigos = [];
 let amigosSorteados = [];
 
+function mostrarMensaje(mensaje) {
+    let mensajeElement = document.getElementById('mensaje');
+    mensajeElement.textContent = mensaje;
+    //Limpiar mensaje despues de 3 segundos
+    setTimeout(() => {
+        mensajeElement.textContent = "";
+    }, 3000);
+}
+
+//funcion para borrar los amigos y volver a uniciar el sorteo
+function borrarAmigos() {
+    //borrar los amigos y amigos sorteados
+    amigos = [];
+    amigosSorteados = [];
+    //limpiar la lista de amigos en el HTML
+    document.getElementById('listaAmigos').innerHTML = "";
+    //limpiar el resultado en el HTML
+    document.getElementById('resultado').textContent = "";
+}
+
 //funcion que agrega un amigo al array amigos
  function agregarAmigo() {
+//validar si el nombre que va a ingresar ya esta en el array y enviar una alerta
+    if (amigos.includes(document.getElementById('amigo').value)) {
+        mostrarMensaje("El amigo ya fue agregado");
+        return false;
+    }   
+
 
     //validar si el nombre de amigo ingresado tiene espacios y enviar una alerta
     if (document.getElementById('amigo').value.includes(" ")) {
-        alert("El nombre no puede contener espacios");
+        mostrarMensaje("El nombre no puede contener espacios");
         return false;
     }
-    
+
     
         //validar si el nombre de amigo ingresado tiene numeros y enviar una alerta
     if (document.getElementById('amigo').value.match(/\d/)) {
-        alert("El nombre no puede contener numeros");
+        mostrarMensaje("El nombre no puede contener numeros");
         return false;
     }
     //validar si el campo input esta vacio y enviar una alerta
     if (document.getElementById('amigo').value == "") {
-        alert("El campo no puede estar vacio");
+        mostrarMensaje("El campo no puede estar vacio");
         return false;
     }
-
     //añadir el nombre del amigo al array
     amigos.push(document.getElementById('amigo').value);
     //despues de añadir el nombre del amigo al array, limpiar el campo input
-    document.getElementById('amigo').value = "";
-    console.log(amigos);
+    document.getElementById('amigo').value = "";  
     mostrarAmigos();
  }
 
@@ -52,12 +76,12 @@ let amigosSorteados = [];
     function sortearAmigo() {
         //validar si el array esta vacio y enviar una alerta
         if (amigos.length == 0) {
-            alert("No hay amigos para seleccionar");
+            mostrarMensaje("No hay amigos para seleccionar");
             return false;
         }
         //validar que el array amigosSorteados sea igual al array amigos e indicar que ya se sortearon todos los amigos
         if (amigosSorteados.length == amigos.length) {
-            alert("Ya se sortearon todos los amigos");
+            mostrarMensaje("Ya se sortearon todos los amigos");
             amigosSorteados = [];
         }
         //seleccionar un amigo aleatorio que no haya sido sorteado usando do while
@@ -69,8 +93,6 @@ let amigosSorteados = [];
         document.getElementById('resultado').textContent = amigos[indice];
         //Añadir el amigo seleccionado al array amigosSorteados
         amigosSorteados.push(indice);
-        console.log(amigosSorteados);
-
         //Seleccionar un amigo de manera aleatoria
         //let indice = Math.floor(Math.random() * amigos.length);
         //Mostrar el nombre usando document.getElementById('resultado').textContent
